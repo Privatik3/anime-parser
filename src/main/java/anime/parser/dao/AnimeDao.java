439388e45216af254e2e28ead4103655667fc51a
Bon4ik <makcharchenko95@gmail.com>
2015-01-01 16:48:05 +0200

    AnimeDao, AnimeDaoImpl upgrate

--- src/main/java/anime/parser/dao/AnimeDao.java
+++ src/main/java/anime/parser/dao/AnimeDao.java
@@ -1,5 +1,20 @@
 package anime.parser.dao;
 
+import anime.parser.domain.*;
+
+import java.sql.SQLException;
+
 public interface AnimeDao {
+
+    public void setAnimeInfoById(AnimesEntity animeInfoById) throws SQLException;
+    public void setAnimesGanresById(AnimeGenreEntity animesGanresById) throws SQLException;
+    public void setAnimeResourcesesById(AnimeResourcesEntity animeResourcesesById) throws SQLException;
+    public void setDirectedByDirectedId(DirectedEntity directedByDirectedId) throws SQLException;
+    public void setSecondNameByOtherTitleId(ScreenshotsEntity secondNameByOtherTitleId) throws SQLException;
+    public void setStudioByStudioId(StudioEntity studioByStudioId)throws SQLException;
+    public void setAnimeTypesByAnimeTypeId(TypesEntity animeTypesByAnimeTypeId)throws SQLException;
+    public void setYearProductionByYearProductionId(YearProductionEntity yearProductionByYearProductionId)throws SQLException;
+    public void setConnectionsesById(ConnectionsEntity connectionsesById)throws SQLException;
+    public void setScreenshotsesById(ScreenshotsEntity screenshotsesById)throws SQLException;
 
 }
--- src/main/java/anime/parser/dao/implement/AnimeDaoImpl.java
+++ src/main/java/anime/parser/dao/implement/AnimeDaoImpl.java
@@ -1,7 +1,166 @@
 package anime.parser.dao.implement;
 
 import anime.parser.dao.AnimeDao;
+import anime.parser.domain.*;
+import anime.parser.utill.HibernateUtil;
+import org.hibernate.Session;
 
-public class AnimeDaoImpl implements AnimeDao {
+import java.sql.SQLException;
 
+public class AnimeDaoImpl implements AnimeDao{
+
+    @Override
+    public void setAnimeInfoById(AnimesEntity animeInfoById) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(animeInfoById);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+    }
+
+    @Override
+    public void setAnimesGanresById(AnimeGenreEntity animesGanresById) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(animesGanresById);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+    }
+
+    @Override
+    public void setAnimeResourcesesById(AnimeResourcesEntity animeResourcesesById) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(animeResourcesesById);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+
+    }
+
+    @Override
+    public void setDirectedByDirectedId(DirectedEntity directedByDirectedId) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(directedByDirectedId);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+
+    }
+
+    @Override
+    public void setSecondNameByOtherTitleId(ScreenshotsEntity secondNameByOtherTitleId) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(secondNameByOtherTitleId);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+
+    }
+
+    @Override
+    public void setStudioByStudioId(StudioEntity studioByStudioId) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(studioByStudioId);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+
+    }
+
+    @Override
+    public void setAnimeTypesByAnimeTypeId(TypesEntity animeTypesByAnimeTypeId) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(animeTypesByAnimeTypeId);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+
+    }
+
+    @Override
+    public void setYearProductionByYearProductionId(YearProductionEntity yearProductionByYearProductionId) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(yearProductionByYearProductionId);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+    }
+
+    @Override
+    public void setConnectionsesById(ConnectionsEntity connectionsesById) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(connectionsesById);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+    }
+
+    @Override
+    public void setScreenshotsesById(ScreenshotsEntity screenshotsesById) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(screenshotsesById);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if ((session != null) && (session.isOpen())) session.close();
+        }
+    }
 }
